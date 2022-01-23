a4ba6980556dabbbcd5e9b8584c286035135a447
Jaswal1p <Jaswal1p@yahoo.com>
2022-01-23 01:04:09 -0500

    successfully captured input

---
+++ app.js
@@ -0,0 +1,35 @@
+const { writeFile, copyFile } = require('fs');
+const inquirer = require('inquirer');
+const generatePage = require('./page-template.js');
+
+const askUser = () => {
+    return inquirer.prompt([
+        {
+           type: 'input',
+           name: 'name',
+           message: 'What is the title of your project (Required)',
+           validate: titleInput => {
+               if (titleInput) {
+                   return true;
+                } else {
+                    console.log('Please enter title of your project');
+                    return false;
+                }
+           }
+        },
+        {
+            type: 'checkbox',
+            name: 'sections',
+            message: 'Please pick sections that you want to include (recommendation: include all of them)',
+            choices: ['Description', 'Table of Contents', 'Installation', 'Usage', 'License', 'Contributing', 'Tests', 'Questions']
+        }
+    ])
+}
+
+askUser()
+.then(portfolioData => {
+    return generatePage(portfolioData);
+})
+.then(pageHTML => {
+    return writeFile(pageHTML);
+})
