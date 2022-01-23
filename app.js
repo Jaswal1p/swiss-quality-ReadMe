6cc847a9bc7f3717081ed562e6561b5414f214e1
Jaswal1p <Jaswal1p@yahoo.com>
2022-01-23 18:26:19 -0500

    commented out entire app.js & page-template(possib htm creation in future)

--- app.js
+++ app.js
@@ -1,147 +1,149 @@
-const { writeFile, copyFile, fstat } = require('fs');
-const inquirer = require('inquirer');
-const generatePage = require('./utils/generateMarkdown.js');
+// const { writeFile, copyFile, fstat } = require('fs');
+// const inquirer = require('inquirer');
+// const generatePage = require('./utils/generateMarkdown.js');
 
-//const pageHTML = generatePage(name, description);
+// //const pageHTML = generatePage(name, description);
 
-// fs.writeFile('./index.html', pageHTML, err => {
-    // if (err) throw err;
+// // fs.writeFile('./index.html', pageHTML, err => {
+//     // if (err) throw err;
 
-    //console.log('page complete! checkout index.html')
-// });
+//     //console.log('page complete! checkout index.html')
+// // });
 
-const askUser = () => {
-    return inquirer.prompt([
-        {
-           type: 'input',
-           name: 'name',
-           message: 'What is the title of your project (Required)',
-           validate: titleInput => {
-               if (titleInput) {
-                   return true;
-                } else {
-                    console.log('Please enter title of your project');
-                    return false;
-                }
-           }
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'describe your project',
+// const askUser = () => {
+//     return inquirer.prompt([
+//         {
+//            type: 'input',
+//            name: 'name',
+//            message: 'What is the title of your project (Required)',
+//            validate: titleInput => {
+//                if (titleInput) {
+//                    return true;
+//                 } else {
+//                     console.log('Please enter title of your project');
+//                     return false;
+//                 }
+//            }
+//         },
+//         {
+//             type: 'input',
+//             name: 'description',
+//             message: 'describe your project',
 
 
-        },
-        {
-            type: 'checkbox',
-            name: 'sections',
-            message: 'Please pick sections that you want to include (recommendation: include all of them)',
-            choices: ['Description', 'Table of Contents', 'Installation', 'Usage', 'License', 'Contributing', 'Tests', 'Questions']
-        }
-    ])
-}
+//         },
+//         {
+//             type: 'checkbox',
+//             name: 'sections',
+//             message: 'Please pick sections that you want to include (recommendation: include all of them)',
+//             choices: ['Description', 'Table of Contents', 'Installation', 'Usage', 'License', 'Contributing', 'Tests', 'Questions']
+//         }
+//     ])
+// }
 
-const askSection = portfolioData => {
-    console.log(`
-    ==================
-    Add a New Section
-    ==================  
-    `);
+// const askSection = portfolioData => {
+//     console.log(`
+//     ==================
+//     Add a New Section
+//     ==================  
+//     `);
   
-    // If there's no 'sections' array property, create one
-    if (!portfolioData.sections) {
-      portfolioData.sections = [];
-    }
+//     // If there's no 'sections' array property, create one
+//     if (!portfolioData.sections) {
+//       portfolioData.sections = [];
+//     }
   
-    return inquirer.prompt([
-      {
-        type: 'input',
-        name: 'Instal',
-        message: 'Please add installation instructions here (Required)',
-        validate: nameInput => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log('You need to enter installation instructions!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'usage',
-        message: 'Provide use case for this project (Required)',
-        validate: usageInput => {
-          if (usageInput) {
-            return true;
-          } else {
-            console.log('You need to provide use case for this project!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'contribution',
-        message: 'Please provide contribution guidelines for this project (Required)',
-        validate: contributionInput => {
-            if (contributionInput) {
-              return true;
-            } else {
-              console.log('You need to provide contribution guidelines!');
-              return false;
-            }
-          }
+//     return inquirer.prompt([
+//       {
+//         type: 'input',
+//         name: 'Instal',
+//         message: 'Please add installation instructions here (Required)',
+//         validate: nameInput => {
+//           if (nameInput) {
+//             return true;
+//           } else {
+//             console.log('You need to enter installation instructions!');
+//             return false;
+//           }
+//         }
+//       },
+//       {
+//         type: 'input',
+//         name: 'usage',
+//         message: 'Provide use case for this project (Required)',
+//         validate: usageInput => {
+//           if (usageInput) {
+//             return true;
+//           } else {
+//             console.log('You need to provide use case for this project!');
+//             return false;
+//           }
+//         }
+//       },
+//       {
+//         type: 'input',
+//         name: 'contribution',
+//         message: 'Please provide contribution guidelines for this project (Required)',
+//         validate: contributionInput => {
+//             if (contributionInput) {
+//               return true;
+//             } else {
+//               console.log('You need to provide contribution guidelines!');
+//               return false;
+//             }
+//           }
   
-      },
-      {
-        type: 'input',
-        name: 'test',
-        message: 'Please provide test instructions for this project (Required)',
-        validate: linkInput => {
-          if (linkInput) {
-            return true;
-          } else {
-            console.log('You need to enter a project GitHub link!');
-            return false;
-          }
-        }
-      },
-    //   {
-    //     type: 'confirm',
-    //     name: 'feature',
-    //     message: 'Would you like to feature this section?',
-    //     default: false
-    //   },
-    //   {
-    //     type: 'confirm',
-    //     name: 'confirmAddProject',
-    //     message: 'Would you like to enter another section?',
-    //     default: false
-    //   }
-    ])
+//       },
+//       {
+//         type: 'input',
+//         name: 'test',
+//         message: 'Please provide test instructions for this project (Required)',
+//         validate: linkInput => {
+//           if (linkInput) {
+//             return true;
+//           } else {
+//             console.log('You need to enter a project GitHub link!');
+//             return false;
+//           }
+//         }
+//       },
+//     //   {
+//     //     type: 'confirm',
+//     //     name: 'feature',
+//     //     message: 'Would you like to feature this section?',
+//     //     default: false
+//     //   },
+//     //   {
+//     //     type: 'confirm',
+//     //     name: 'confirmAddProject',
+//     //     message: 'Would you like to enter another section?',
+//     //     default: false
+//     //   }
+//     ])
 
-    .then(sectionData => {
-        portfolioData.sections.push(sectionData);
+//     .then(sectionData => {
+//         portfolioData.sections.push(sectionData);
         
-    });
-}
+//     });
+// }
 
-askUser()
-.then(askSection)
-.then(portfolioData => {
-    return generatePage(portfolioData);
-})
-.then(pageHTML => {
-    return writeFile(pageHTML);
-})
-.then(writeFileResponse => {
-    console.log(writeFileResponse);
-    return copyFile();
-})
-.then(copyFileResponse => {
-    console.log(copyFileResponse);
-})
-.catch(err => {
-    console.log(err);
-});
+// askUser()
+// .then(askSection)
+
+// writeFile('readme', generateMarkdown );
+// .then(portfolioData => {
+//     return generatePage(portfolioData);
+// })
+// .then(pageHTML => {
+//     return writeFile(pageHTML);
+// })
+// .then(writeFileResponse => {
+//     console.log(writeFileResponse);
+//     return copyFile();
+// })
+// .then(copyFileResponse => {
+//     console.log(copyFileResponse);
+// })
+// .catch(err => {
+//     console.log(err);
+// });
--- src/page-template.js
+++ src/page-template.js
@@ -1,134 +1,134 @@
-const { table } = require("console");
+// const { table } = require("console");
 
-// create table of contents
+// // create table of contents
 
-const generateTable = tableText => {
-    return `
-    <section class="my-3" id="about">
-       <he class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
-       <p>${tableText}</p>
+// const generateTable = tableText => {
+//     return `
+//     <section class="my-3" id="about">
+//        <he class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+//        <p>${tableText}</p>
     
-    `;
-}  
+//     `;
+// }  
 
-// create the about section
-const generateAbout = aboutText => {
-    if (!aboutText) {
-      return '';
-    }  
+// // create the about section
+// const generateAbout = aboutText => {
+//     if (!aboutText) {
+//       return '';
+//     }  
   
-  return `
-    <section class="my-3" id="about">
-      <he class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
-      <p>${aboutText}</p>
-    `;
-}    
+//   return `
+//     <section class="my-3" id="about">
+//       <he class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+//       <p>${aboutText}</p>
+//     `;
+// }    
 
-const generateInstal = instalText => {
-    if (!aboutText) {
-        return '';
-    }
+// const generateInstal = instalText => {
+//     if (!aboutText) {
+//         return '';
+//     }
 
-    return `
-    <section class="my-3" id="about">
-       <he class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
-       <p>${instalText}</p>
+//     return `
+//     <section class="my-3" id="about">
+//        <he class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+//        <p>${instalText}</p>
     
-    `;
-}
+//     `;
+// }
 
 
  
   
-// const generateProjects = projectsArr => {
-    // return `
-    //   <section class="my-3" id="portfolio">
-        // <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-        // <div class="flex-row justify-space-between">
-        // ${projectsArr
-        //   .filter(({ feature }) => feature)
-        //   .map(({ name, description, languages, link }) => {
-        //     return `
-        //     <div class="col-12 mb-2 bg-dark text-light p-3">
-        //       <h3 class="portfolio-item-title text-light">${name}</h3>
-        //       <h5 class="portfolio-languages">
-        //         Built With:
-        //         ${languages.join(', ')}
-        //       </h5>
-        //       <p>${description}</p>
-        //       <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-        //     </div>
-        //   `;
-        //   })
-        //   .join('')}
+// // const generateProjects = projectsArr => {
+//     // return `
+//     //   <section class="my-3" id="portfolio">
+//         // <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+//         // <div class="flex-row justify-space-between">
+//         // ${projectsArr
+//         //   .filter(({ feature }) => feature)
+//         //   .map(({ name, description, languages, link }) => {
+//         //     return `
+//         //     <div class="col-12 mb-2 bg-dark text-light p-3">
+//         //       <h3 class="portfolio-item-title text-light">${name}</h3>
+//         //       <h5 class="portfolio-languages">
+//         //         Built With:
+//         //         ${languages.join(', ')}
+//         //       </h5>
+//         //       <p>${description}</p>
+//         //       <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+//         //     </div>
+//         //   `;
+//         //   })
+//         //   .join('')}
   
-//         ${projectsArr
-//           .filter(({ feature }) => !feature)
-//           .map(({ name, description, languages, link }) => {
-//             return `
-//             <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-//               <h3 class="portfolio-item-title text-light">${name}</h3>
-//               <h5 class="portfolio-languages">
-//                 Built With:
-//                 ${languages.join(', ')}
-//               </h5>
-//               <p>${description}</p>
-//               <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-//             </div>
-//           `;
-//           })
-//           .join('')}
-//         </div>
-//       </section>
-//     `;
-//   };
+// //         ${projectsArr
+// //           .filter(({ feature }) => !feature)
+// //           .map(({ name, description, languages, link }) => {
+// //             return `
+// //             <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+// //               <h3 class="portfolio-item-title text-light">${name}</h3>
+// //               <h5 class="portfolio-languages">
+// //                 Built With:
+// //                 ${languages.join(', ')}
+// //               </h5>
+// //               <p>${description}</p>
+// //               <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+// //             </div>
+// //           `;
+// //           })
+// //           .join('')}
+// //         </div>
+// //       </section>
+// //     `;
+// //   };
   
-  module.exports = templateData => {
-  // // destructure page data by section
-  const { table, about, ...header } = templateData;
-  
-  
-      return `
-      <!DOCTYPE html>
-      <html lang="en"> 
+//   module.exports = templateData => {
+//   // // destructure page data by section
+//   const { table, about, ...header } = templateData;
   
   
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Portfolio Demo</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-        <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="style.css">
-      </head>
+//       return `
+//       <!DOCTYPE html>
+//       <html lang="en"> 
+  
+  
+//       <head>
+//         <meta charset="UTF-8">
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//         <title>Portfolio Demo</title>
+//         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+//         <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+//         <link rel="stylesheet" href="style.css">
+//       </head>
     
-      <body>
-        <header>
-          <div class="container flex-row justify-space-between align-center py-3">
-             <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
-             <nav class="flex-row">
-             <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.badge}">License</a> 
-             </nav>
-          </div>
-        </header>   
+//       <body>
+//         <header>
+//           <div class="container flex-row justify-space-between align-center py-3">
+//              <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
+//              <nav class="flex-row">
+//              <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.badge}">License</a> 
+//              </nav>
+//           </div>
+//         </header>   
   
-        <main class="container my-5">
-           <div class="container flex-row justify-space-between align-center py-3">
-              <h2 class="page-title text-secondary bg-dark py-2 px-3">Table of Contents:${header.name}</h2>
-              ${generateTable(table)}
-           </div>
+//         <main class="container my-5">
+//            <div class="container flex-row justify-space-between align-center py-3">
+//               <h2 class="page-title text-secondary bg-dark py-2 px-3">Table of Contents:${header.name}</h2>
+//               ${generateTable(table)}
+//            </div>
 
-           ${generateAbout(about)}
-           ${generateInstal(installation)}
+//            ${generateAbout(about)}
+//            ${generateInstal(installation)}
            
-        </main>
+//         </main>
   
-        <footer class="container text-center py-3">
-            <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
-        </footer>
+//         <footer class="container text-center py-3">
+//             <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+//         </footer>
   
-      </body>
-      </html>
-      `;
-    }; 
+//       </body>
+//       </html>
+//       `;
+//     }; 
