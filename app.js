fe313255b2ea8f4e0c0bc4f662e037e7d562a137
Jaswal1p <Jaswal1p@yahoo.com>
2022-01-23 14:33:38 -0500

    added code to app.js and page-template to aquire input and display in sections

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ const askUser = () => {
            }
         },
         {
+            type: 'input',
+            name: 'description',
+            message: 'describe your project',
+
+
+        },
+        {
             type: 'checkbox',
             name: 'sections',
             message: 'Please pick sections that you want to include (recommendation: include all of them)',
@@ -26,8 +33,107 @@ const askUser = () => {
     ])
 }
 
+const askSection = portfolioData => {
+    console.log(`
+    ==================
+    Add a New Section
+    ==================  
+    `);
+  
+    // If there's no 'sections' array property, create one
+    if (!portfolioData.sections) {
+      portfolioData.sections = [];
+    }
+  
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'Instal',
+        message: 'Please add installation instructions here (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('You need to enter installation instructions!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide use case for this project (Required)',
+        validate: usageInput => {
+          if (usageInput) {
+            return true;
+          } else {
+            console.log('You need to provide use case for this project!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please provide contribution guidelines for this project (Required)',
+        validate: contributionInput => {
+            if (contributionInput) {
+              return true;
+            } else {
+              console.log('You need to provide contribution guidelines!');
+              return false;
+            }
+          }
+  
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'Please provide test instructions for this project (Required)',
+        validate: linkInput => {
+          if (linkInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project GitHub link!');
+            return false;
+          }
+        }
+      },
+    //   {
+    //     type: 'confirm',
+    //     name: 'feature',
+    //     message: 'Would you like to feature this section?',
+    //     default: false
+    //   },
+    //   {
+    //     type: 'confirm',
+    //     name: 'confirmAddProject',
+    //     message: 'Would you like to enter another section?',
+    //     default: false
+    //   }
+    ])
+
+    .then(sectionData => {
+        portfolioData.sections.push(sectionData);
+        
+    });
+}
+
 askUser()
+.then(askSection)
 .then(portfolioData => {
     return generatePage(portfolioData);
 })
-
+.then(pageHTML => {
+    return writeFile(pageHTML);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+})
+.then(copyFileResponse => {
+    console.log(copyFileResponse);
+})
+.catch(err => {
+    console.log(err);
+});
